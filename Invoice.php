cde5feafc344736d36d812cc35fddf0fba2b768f
t_kjaergaard <tkj@tkjaergaard.dk>
2014-02-12 14:39:40 +0100

    Refactor

--- Invoice.php
+++ Invoice.php
@@ -1,7 +1,19 @@
 <?php namespace Boutique\Invoice;
 
-class Invoice implements InvoiceInterface {
+abstract class Invoice {
 
+    /**
+     * Invoice repository
+     * @var Boutique\Invoice\InvoiceInterface
+     */
+    protected $invoice;
 
-
+    /**
+     * Class constructor
+     * @param InvoiceInterface $invoice
+     */
+    public function __construct(InvoiceInterface $invoice)
+    {
+        $this->invoice = $invoice;
+    }
 }
--- InvoiceInterface.php
+++ InvoiceInterface.php
@@ -2,6 +2,28 @@
 
 interface InvoiceInterface {
 
+    /**
+     * Set a invoice as precessable
+     * @return boolean
+     */
+    public function process();
 
+    /**
+     * Set a invoice as refunded
+     * @return boolean
+     */
+    public function refund();
+
+    /**
+     * Set a invoice as canceled
+     * @return boolean
+     */
+    public function cancel();
+
+    /**
+     * Set a invoice as completed
+     * @return booelan
+     */
+    public function completed();
 
 }
